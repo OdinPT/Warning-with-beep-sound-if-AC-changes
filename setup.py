378b7059a1d38232b7da47223e60f60b3a381277
OdinPT <leonardo.almeidavieira@gmail.com>
2018-11-01 19:31:42 +0000

    __PT___ A guardar dados no ficheiro de configuração
    __EN__
    Saving data to the configuration file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'Avisa_modo_bateria',
+    version = '0.1',
+    description = 'An example of an installable program',
+    author = 'OdinPT',
+    license = 'GPL',
+    entry_points = {'console_scripts': ['prog = Avisa_Bat.py',],},
+)
